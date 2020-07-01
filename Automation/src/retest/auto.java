96bcf0840f4fb4aa48562cff2f1ba085da8213b6
prachisarade <prachisarade@localhost>
2020-07-01 12:50:09 +0530

    test

--- Automation/src/retest/auto.java
+++ Automation/src/retest/auto.java
@@ -1,5 +1,15 @@
 package retest;
 
 public class auto {
+	public static void main(String[] args) {
+	//
+		
+		
+		
+		System.out.println("Hello World");
+		System.out.println("Hello");
+		
+
+	}
 
 }
